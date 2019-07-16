ae23224f888777ac52cf3729f4e797bf2b139df4
novakvova <novakvova@gmail.com>
2019-07-16 12:22:50 +0300

    Base app API

---
+++ BackendAndroid/DAL/Entities/SeederDB.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackendAndroid.DAL.Entities
+{
+    public static class SeederDB
+    {
+        private static void SeedUsers(UserManager<DbUser> userManager,
+                RoleManager<DbRole> roleManager)
+        {
+            var roleName = "Admin";
+            if (roleManager.FindByNameAsync(roleName).Result == null)
+            {
+                var result = roleManager.CreateAsync(new DbRole
+                {
+                    Name = roleName
+                }).Result;
+            }
+
+            if (userManager.FindByEmailAsync("admin@gmail.com").Result == null)
+            {
+                string email = "admin@gmail.com";
+                
+                var user = new DbUser
+                {
+                    Email = email,
+                    UserName = email,
+                    PhoneNumber = "+38(666)666-66-66"
+                };
+                var result = userManager.CreateAsync(user, "Qwerty1-").Result;
+                result = userManager.AddToRoleAsync(user, roleName).Result;
+            }
+        }
+
+        public static void SeedData(IServiceProvider services, 
+            IHostingEnvironment env, IConfiguration config)
+        {
+            using (var scope = services.GetRequiredService<IServiceScopeFactory>().CreateScope())
+            {
+                var manager = scope.ServiceProvider.GetRequiredService<UserManager<DbUser>>();
+                var managerRole = scope.ServiceProvider.GetRequiredService<RoleManager<DbRole>>();
+                var context = scope.ServiceProvider.GetRequiredService<EFDbContext>();
+                SeederDB.SeedUsers(manager, managerRole);
+            }
+        }
+    }
+}
+    
---
+++ BackendAndroid/Startup.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BackendAndroid.DAL.Entities;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+
+namespace BackendAndroid
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            
+
+            services.AddDbContext<EFDbContext>(opt =>
+               opt.UseSqlServer(Configuration
+                    .GetConnectionString("DefaultConnection")));
+
+            services.AddIdentity<DbUser, DbRole>()
+                .AddEntityFrameworkStores<EFDbContext>();
+
+            var signingKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("this is the secret phrase"));
+
+            services.Configure<IdentityOptions>(options =>
+            {
+                // Default Password settings.
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequiredUniqueChars = 0;
+            });
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(cfg =>
+            {
+                cfg.RequireHttpsMetadata = false;
+                cfg.SaveToken = true;
+                cfg.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    IssuerSigningKey = signingKey,
+                    ValidateAudience = false,
+                    ValidateIssuer = false,
+                    ValidateLifetime = false,
+                    ValidateIssuerSigningKey = true
+                };
+            });
+
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseAuthentication();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+
+           //SeederDB.SeedData(app.ApplicationServices, env, this.Configuration);
+        }
+    }
+}
